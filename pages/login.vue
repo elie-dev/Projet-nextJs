4f7c9ecea8bfa7fc26566ff15ecbb5269ae92fe8
Elie Guilbaud <elie.guilbaud@sfr.fr>
2021-10-08 09:00:23 +0200

    Mise en place du localStorage

--- pages/login.vue
+++ pages/login.vue
@@ -5,10 +5,9 @@
     lazy-validation
   >
     <v-text-field
-      v-model="name"
-      :counter="10"
-      :rules="nameRules"
-      label="Name"
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
       required
     />
 
@@ -54,13 +53,18 @@
 </template>
 
 <script>
+
+  import NxLocalForage from 'vue-localstorage';
+  import Vue from 'vue';
+
   export default {
+
     data: () => ({
       valid: true,
-      name: '',
-      nameRules: [
-        v => !!v || 'Name is required',
-        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
       ],
       password: '',
       passwordRules: [
@@ -72,7 +76,27 @@
 
     methods: {
       validate () {
-        this.$refs.form.validate()
+        if(this.$refs.form.validate()) {
+          Vue.use(NxLocalForage, {
+            name: 'ls',
+            bind: true //created computed members from your variable declarations
+          })
+
+          // Get data from localStorage
+          let data = Vue.ls.get('users')
+          if (data === null) {
+            data = {}
+            Vue.ls.set('users', JSON.stringify(data))
+          } else {
+            data = JSON.parse(data)
+          }
+
+          if(data[this.email] !== undefined && data[this.email].password === this.password) {
+            console.log('oui')
+          } else {
+            console.log('non')
+          }
+        }
       },
       reset () {
         this.$refs.form.reset()
